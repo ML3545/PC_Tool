6400e77fab1ab8c0d329463e532ae431dbcaf7e0
meir3545 <jkd3545@gmail.com>
2024-08-21 17:00:34 +0300

    trying to search softare instllation

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from tkinter.constants import DISABLED
 
-from Downlods_handler import print_checkbox_status
-from win_settings_handler import apply_settings
+from Handlers.Downlods_handler import print_checkbox_status, check_software_installed
+from Handlers.win_settings_handler import apply_settings
 
 checkboxes = {}
 checkbox_var = {}
@@ -12,27 +13,31 @@ def create_GUI():
     root.title("MEIR PC Tool")
     downloads(root)
     settings(root)
+    root.geometry('300x300')
     root.mainloop()
-    # root.geometry('600x600')
     # frm = ttk.Frame(root)
     # frm.pack(padx=10, pady=10)
     # frm.grid()
     # downloads(root)
     # settings(frm)
 
-
 def downloads(root):
     # cb_chrome = ttk.Checkbutton(frm, text="Chrome", variable=var).grid(column=0, row=0, padx=10, pady=3)
     # cb_vlc = ttk.Checkbutton(frm, text="VLC Pleyar").grid(column=0, row=1, padx=10, pady=3)
 
     # Create checkboxes
-    packages = ["Chrome", "VLC Player"]  # Add your package names here
+    packages = ["Google Chrome", "VLC Media Player"]  # Add your package names here
     for package_name in packages:
         var = tk.IntVar(value=0)
         checkbox_var[package_name] = var
         checkbox = tk.Checkbutton(root, text=package_name, variable=var)
         checkboxes[checkbox] = package_name
         checkbox.pack(anchor=tk.W)
+        check_software_installed(package_name)
+        # check_software_installed(package_name)
+        # if package_name:
+        #     tk.Checkbutton.config(state=DISABLED)
+
 
     btn_install = tk.Button(root, text="Install", command=lambda: print_checkbox_status(checkboxes, checkbox_var))
     btn_install.pack()
@@ -40,7 +45,7 @@ def downloads(root):
 
 def settings(root):
     btn_enable_settings = tk.Button(root, text="Enable Settings", command=apply_settings)
-    btn_enable_settings.pack(anchor=tk.W)
+    btn_enable_settings.pack()
 
 
 def main():
